b98db0b4849c641dffef2526216c701dee3b4efc
techinalhamza <techinalhamza@gmail.com>
2024-04-30 01:42:12 +0500

    e

--- data.js
+++ data.js
@@ -3,21 +3,25 @@ const data = [
     productName: "Red Printed T-Shirt",
     price: 50.0,
     image: "images/product-1.jpg",
+    feature: true,
   },
   {
     productName: "HRX Sport Shoes",
     price: 75.0,
     image: "images/product-2.jpg",
+    feature: true,
   },
   {
     productName: "HRX grey track-point",
     price: 45.0,
     image: "images/product-3.jpg",
+    feature: true,
   },
   {
     productName: "Blue Printed T-Shirt",
     price: 50.0,
     image: "images/product-4.jpg",
+    feature: true,
   },
   {
     productName: "grey army shose",
@@ -62,13 +66,45 @@ const data = [
 ];
 
 const featureCategory = document.querySelector(".feature-category");
+const allProducts = document.querySelector(".all-products");
+const featureProducts = document.querySelector(".featureProducts");
 const cartResponsive = document.querySelector(".cart_responsive");
 const cartContaineer = document.querySelector(".cart-containeer");
 const cartCount = document.querySelector(".cart-count");
 var cart = [];
+// featureCategory.style.background = "red";
 
+// featureCategory.addEventListener("click", () => alert("helo"));
+
+// add featureProducts products dynamicaly
+
+const featureProductsList = () => {
+  const fProducts = data.filter((val) => val.feature === true);
+  if (featureProducts) {
+    clutter = "";
+    fProducts.map((val, index) => {
+      return (clutter += `<div class="feature-item">
+                <div class="img">
+                <img src=${val.image} alt=""></div>
+                <div class="feature-content">
+                    <h3>${val.productName}</h3>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star-half-stroke"></i>
+                    <p>${"$" + val.price}</p>
+                    <button data-index=${index} class='addtocart'>add to cart</button>
+                    </div>
+                    </div>`);
+    });
+    featureProducts.innerHTML = clutter;
+  }
+};
+
+// add products dynamically in shop page
 const addProducts = () => {
-  if (featureCategory) {
+  if (allProducts) {
     var clutter = "";
     data.map(
       (val, index) =>
@@ -87,13 +123,17 @@ const addProducts = () => {
                 </div>
             </div>`)
     );
-    featureCategory.innerHTML = clutter;
+    allProducts.innerHTML = clutter;
   }
 };
+
+// added to cart funcationality
 const addToCart = () => {
   if (featureCategory) {
     featureCategory.addEventListener("click", (e) => {
+      alert("hh");
       if (e.target.classList.contains("addtocart")) {
+        console.log(e.target);
         const clickedProductIndex = e.target.dataset.index;
         const clickProduct = data[clickedProductIndex];
         cart.push({ ...data[e.target.dataset.index], quantity: 1 });
@@ -104,16 +144,46 @@ const addToCart = () => {
   }
 };
 
+// accessing cartItems from localStorage
+
+// accessing cartItems from localStorage
 const getItemFromLocalStorage = () => {
   const getItem = localStorage.getItem("cartItem");
   if (getItem) {
     let carrt = JSON.parse(getItem);
+    // Update quantity values for each item in the cart from local storage
+    carrt.forEach((item) => {
+      const index = cart.findIndex((i) => i.productName === item.productName);
+      if (index !== -1) {
+        cart[index].quantity = item.quantity;
+      }
+    });
     updateCartCount();
     return carrt;
   } else {
     return [];
   }
 };
+
+// const getItemFromLocalStorage = () => {
+//   const getItem = localStorage.getItem("cartItem");
+//   if (getItem) {
+//     let carrt = JSON.parse(getItem);
+//     carrt.forEach((item) => {
+//       const index = cart.findIndex((i) => i.productName === item.productName);
+//       if (index !== -1) {
+//         cart[index].quantity = item.quantity;
+//       }
+//     });
+
+//     updateCartCount();
+//     return carrt;
+//   } else {
+//     return [];
+//   }
+// };
+
+// show products in cart dynamically
 
 const showProductsInCart = () => {
   if (cartResponsive) {
@@ -165,6 +235,8 @@ const showProductsInCart = () => {
   }
 };
 
+// cartCount functionality
+
 const updateCartCount = () => {
   if (cartCount) {
     const getCartCount = localStorage.getItem("cartItem");
@@ -175,20 +247,27 @@ const updateCartCount = () => {
   }
 };
 
+// removeItems funcationality
+
 const attachRemoveItemEvent = () => {
   const removeButtons = document.querySelectorAll(".remove-item");
   removeButtons.forEach((button) => {
     button.addEventListener("click", (e) => {
       const index = parseInt(e.target.dataset.index);
       const filterCart = cart.filter((val, ind) => ind !== index);
-      localStorage.setItem("cartItem", JSON.stringify(filterCart));
       cart = filterCart;
+      localStorage.setItem("cartItem", JSON.stringify(cart));
       showProductsInCart();
       updateCartCount();
+      calculateSubtotal();
+      updateQty();
+
       console.log(filterCart);
     });
   });
 };
+
+// update quantity funcationality
 
 const updateQty = () => {
   const arrowUp = document.querySelectorAll(".arrowUp");
@@ -209,8 +288,8 @@ const updateQty = () => {
         .querySelector(".item_price label");
       const newPrice = product.quantity * product.price;
       priceLabel.textContent = "$" + newPrice.toFixed(0); // Assuming price is stored as a decima
-
       localStorage.setItem("cartItem", JSON.stringify(cart));
+      calculateSubtotal();
     });
   });
 
@@ -229,24 +308,54 @@ const updateQty = () => {
           .querySelector(".item_price label");
         const newPrice = product.quantity * product.price;
         priceLabel.textContent = "$" + newPrice.toFixed(0); // Assuming price is stored as a decimal
+        localStorage.setItem("cartItem", JSON.stringify(cart));
+        calculateSubtotal();
       }
     });
   });
 };
 
+// clear cart funcationality
+
 const clearCompCart = () => {
   const clearCart = document.querySelector(".clear-cart");
-  clearCart.addEventListener("click", () => {
-    localStorage.removeItem("cartItem");
-    cart = [];
+  if (clearCart) {
+    clearCart.addEventListener("click", () => {
+      localStorage.removeItem("cartItem");
+      cart = [];
+      showProductsInCart();
+      calculateSubtotal();
+    });
+  }
+};
 
-    showProductsInCart();
-  });
+// subtotal funcationality
+const calculateSubtotal = () => {
+  // const subtotal_ = document.querySelector(".subtotal_");
+  const checkoutTotal = document.querySelector(".checkout-total");
+  const checkoutSubTotal = document.querySelector(".checkout-subtotal");
+  let subtotal = 0;
+
+  if (cart.length > 0) {
+    cart.forEach((item) => {
+      subtotal += item.price * item.quantity;
+    });
+  } else {
+    subtotal = 0;
+  }
+  // subtotal_.textContent = subtotal;
+  checkoutTotal.textContent = "$" + subtotal;
+  const shippingFee = subtotal * 0.05;
+  const shippingFeeWithTex = subtotal + shippingFee;
+
+  checkoutSubTotal.textContent = "$" + shippingFeeWithTex;
 };
 
 var cart = getItemFromLocalStorage();
 showProductsInCart();
 addProducts();
+featureProductsList();
 addToCart();
 updateQty();
 clearCompCart();
+calculateSubtotal();
